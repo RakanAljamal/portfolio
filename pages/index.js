edff7933969de337074150a3d44f8348a68f74d9
Rakan <rakan.aljamal@ww.com>
2021-09-22 23:49:48 +0300

    SEO 101

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,20 @@ export default function Home({ initialState }) {
                       rel="stylesheet"/>
                 <title>Rakan Portfolio</title>
                 <meta name="description" content="This is a portfolio written in NextJS"/>
-                <meta name="keywords" content="rakan, rakan portfolio, rakan work, rakan dev, rakan nextjs, rakan aljamal, Rakan Aljamal, Rakan Aljamal Work"/>
+                <meta name="keywords"
+                      content="rakan, rakan portfolio, rakan work, rakan dev, rakan nextjs, rakan aljamal, Rakan Aljamal, Rakan Aljamal Work"/>
                 <meta name="author" content="Rakan Aljamal"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="../static/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="../static/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon-16x16.png"/>
                 <link rel="manifest" href="../static/site.webmanifest"/>
+                <meta property="og:title" content="Rakan Portfolio"/>
+                <meta property="og:image" content="https://i.ibb.co/8zNLzcX/image.png"/>
+                <meta property="og:image:type" content="image/png"/>
+                <meta property="og:image:width" content="488"/>
+                <meta property="og:image:height" content="789"/>
+                <meta property="og:description" content="This is a website built in nextjs"/>
+
             </Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
             <Particles
