ed6c593d990b238462a72d858d6860e8359ddb7b
Rakan <rakan.aljamal@ww.com>
2021-09-22 16:32:28 +0300

    Adding source code container

--- components/Screen/index.js
+++ components/Screen/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import githubLogo from '../../public/github.png';
 import linkedInLogo from '../../public/linkedin.png';
+import hackerRankLogo from '../../public/hackerRank.png';
 import Image from 'next/image';
 
 const Screen = ({ children }) => {
@@ -22,16 +23,30 @@ const Screen = ({ children }) => {
 
                     <div className={styles.codeScreenCTA}>
 
-                            <div className={styles.imageContainer}>
-                                <Image width={50} height={50} src={githubLogo} alt="github"/>
-                            </div>
+                        <div className={styles.sourceCodeContainer}>
                             <div className={styles.sourceCode}>
-                                <a rel="noreferrer"  target="_blank" href="https://github.com/RakanAljamal/portfolio">Source Code</a>
-                            </div>
-                            <div className={styles.imageContainer}>
-                                <Image width={50} height={50} src={linkedInLogo} alt="linkedin"/>
+                                <a rel="noreferrer" target="_blank" href="https://github.com/RakanAljamal/portfolio">Source
+                                    Code</a>
                             </div>
                         </div>
+                        <div>
+                            <div className={styles.imageContainer}>
+                                <a rel="noreferrer" href="https://www.github.com/RakanAljamal" target="_blank">
+                                    <Image width={25} height={25} src={githubLogo} alt="github"/>
+                                </a>
+                            </div>
+                            <div className={styles.imageContainer}>
+                                <a rel="noreferrer" href="https://www.hackerrank.com/rakanaljamal" target="_blank">
+                                    <Image width={35} height={35} src={hackerRankLogo} alt="hackerRank"/>
+                                </a>
+                            </div>
+                            <div className={styles.imageContainer}>
+                                <a rel="noreferrer" href="https://www.linkedin.com/in/RakanAljamal" target="_blank">
+                                    <Image width={25} height={25} src={linkedInLogo} alt="linkedin"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
